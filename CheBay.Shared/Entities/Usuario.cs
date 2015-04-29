745d85807ca5e3cacfc7e9f33fb6b5f59067d047
Diego Batista <digo1086@hotmail.com>
2015-04-28 21:46:37 -0300

    Entidades

---
+++ CheBay.Shared/Entities/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheBay.Shared.Entities
+{
+    public class Usuario
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string mail { get; set; }
+        public string imagen { get; set; }
+        public string contrasena { get; set; }
+        public float reputacion { get; set; }
+        public int Id { get; set; }
+    }
+}
