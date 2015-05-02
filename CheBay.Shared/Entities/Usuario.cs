a84634ba3e75574183307300d28fa89eb9bd4980
EugeniaVelazco <eugenia.velazco@hotmail.com>
2015-05-02 19:50:34 -0300

    cambios en usuario

--- CheBay.Shared/Entities/Usuario.cs
+++ CheBay.Shared/Entities/Usuario.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace CheBay.Shared.Entities
 {
+    [Table("Usuarios")]
     public class Usuario
     {
+        [Required]
         public string nombre { get; set; }
+        [Required]
         public string apellido { get; set; }
+        [Required]
         public string mail { get; set; }
         public string imagen { get; set; }
-        public string contrasena { get; set; }
-        public float reputacion { get; set; }
-        public int Id { get; set; }
+        [Required]
+        public string contrasena { get; set; }       
+        public float reputacion { get; set; }      
     }
 }
