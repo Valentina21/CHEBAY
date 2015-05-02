428ee4814f1a5ae700725e32c64e870030a6e63d
EugeniaVelazco <eugenia.velazco@hotmail.com>
2015-05-02 20:23:38 -0300

    aa

--- CheBay.Shared/Entities/Usuario.cs
+++ CheBay.Shared/Entities/Usuario.cs
@@ -8,17 +8,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CheBay.Shared.Entities
 {
-    [Table("Usuarios")]
     public class Usuario
     {
-        [Required]
         public string nombre { get; set; }
-        [Required]
         public string apellido { get; set; }
-        [Required]
         public string mail { get; set; }
         public string imagen { get; set; }
-        [Required]
         public string contrasena { get; set; }
         public float reputacion { get; set; }
         public int Id { get; set; }
