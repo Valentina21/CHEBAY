3c463f22e78e31fa4fe9df7c3883a15195530cca
agustin83 <agusandrade@live.com>
2015-05-05 21:48:42 -0300

    df

---
+++ CheBay.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using CheBay.Shared.Entities;
+using CheBay.Logic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheBay.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tienda t = new Tienda();
+            t.nombre = "Ebay"; 
+            t.slogan = "El mejor local de ventas";
+            t.tema = "azul";
+            t.basededatos = "Ebay";
+            t.idadministrador = "agustin";
+            LogicLayer.AddTienda(t);
+        }
+    }
+}
---
+++ CheBay.Data/DataLayer.cs
@@ -0,0 +1,20 @@
+﻿using CheBay.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheBay.Data
+{
+    public static class DataLayer
+    {
+        public static void AddTienda(Tienda tienda)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                context.Tiendas.Add(tienda);
+            }
+        }
+    }  
+}
