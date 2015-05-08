78929ac888ab82c7ace7a0808d0e33a7cdfb0bc8
nachogol171 <nachoort@gmail.com>
2015-05-07 22:22:59 -0300

    ultima

--- CheBay.Shared/Entities/Tienda.cs
+++ CheBay.Shared/Entities/Tienda.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,25 +8,10 @@ namespace CheBay.Shared.Entities
 {
     public class Tienda
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int Id { get; set; }
-        [Required]
         public string nombre { get; set; }
-        [Required]
-        public string idadministrador { get; set; }
-        [Required]
+        public int idadministrador { get; set; }
         public string slogan { get; set; }
-        [Required]
         public string tema { get; set; }
-        [Required]
         public string basededatos { get; set; }
     }
-
-
-    public class InheritanceMappingContext : DbContext
-    {
-        public DbSet<Tienda> Tiendas { get; set; }
-    }
-
-
 }
