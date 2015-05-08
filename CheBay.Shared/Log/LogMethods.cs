1e5a62fda465bef7b831df0ff8635c0f2fae6008
Diego Batista <digo1086@hotmail.com>
2015-05-07 21:23:12 -0300

    paypal

---
+++ CheBay.Shared/Log/LogMethods.cs
@@ -0,0 +1,64 @@
+﻿using CheBay.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheBay.Shared.Log
+{
+    public static class LogMethods
+    {
+        public static void AddLog(string message, LogsTypes lt)
+        {
+            int method = int.Parse(ConfigurationSettings.AppSettings["logType"]);
+            switch (method)
+            {
+                case 0:
+                    AddEventViewerLog(message, lt);
+                    break;
+                case 1:
+                    AddEventTextFile(message, lt);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static void AddEventViewerLog(string message, LogsTypes lt)
+        {
+            EventLogEntryType ltEv = EventLogEntryType.Error;
+
+            if (lt == LogsTypes.Information)
+            {
+                ltEv = EventLogEntryType.Information;
+            }
+            else if (lt == LogsTypes.Warning)
+            {
+                ltEv = EventLogEntryType.Warning;
+            }
+
+            string sc = "EmployeeApp";
+            if (!EventLog.SourceExists(sc))
+            {
+                EventLog.CreateEventSource(sc, sc);
+            }
+
+            EventLog.WriteEntry(sc, message, ltEv, 100);
+        }
+
+        private static void AddEventTextFile(string message, LogsTypes lt)
+        {
+            string lines = DateTime.Now.ToShortDateString() + " - " + lt.ToString() + " - " + message;
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(ConfigurationSettings.AppSettings["pathLogFile"]);
+            file.WriteLine(lines);
+
+            file.Close();
+        }
+
+    }
+}
