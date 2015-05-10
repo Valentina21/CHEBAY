abf04bfc6c5277e4eeb5572ae5ce198be11d69c6
agustin83 <agusandrade@live.com>
2015-05-10 12:59:50 -0300

    data

--- CheBay.Data/DataLayer.cs
+++ CheBay.Data/DataLayer.cs
@@ -14,7 +14,64 @@ namespace CheBay.Data
             using (var context = new InheritanceMappingContext())
             {
                 context.Tiendas.Add(tienda);
+
+                context.SaveChanges();
             }
         }
+
+        #region Atributos
+
+        public static List<Atributo> GetAtributos()
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                return context.Atributos.ToList();
+            }
+        }
+
+        public static void DeleteAtributo(int id)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                Atributo atr = GetAtributo(id);
+                context.Atributos.Attach(atr);
+                context.Atributos.Remove(atr);
+                context.SaveChanges();
+            }
+        }
+
+        public static void UpdateAtributo(Atributo atr)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                context.Atributos.Attach(atr);
+                context.SaveChanges();
+            }
+        }
+
+        public static Atributo GetAtributo(int id)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                if (context.Atributos.Where(p => p.id == id).Count() > 0)
+                {
+                    return context.Atributos.First(p => p.id == id);
+                }
+                return null;
+            }
+        }
+
+        public static void AddAtributo(Atributo atr)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                context.Atributos.Add(atr);
+
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
+
     }  
 }
