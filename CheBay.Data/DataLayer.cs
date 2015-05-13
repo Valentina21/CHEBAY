4e3e4b3d8987a9bfff0ba950875498b6672c4b91
nachogol171 <nachoort@gmail.com>
2015-05-12 22:00:30 -0300

    si

--- CheBay.Data/DataLayer.cs
+++ CheBay.Data/DataLayer.cs
@@ -9,15 +9,30 @@ namespace CheBay.Data
 {
     public static class DataLayer
     {
+        #region Tienda
+
         public static void AddTienda(Tienda tienda)
         {
             using (var context = new InheritanceMappingContext())
             {
                 context.Tiendas.Add(tienda);
-
                 context.SaveChanges();
             }
         }
+
+        public static Tienda GetTiendaByName(string name)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                if (context.Tiendas.Where(p => p.nombre == name).Count() > 0)
+                {
+                    return context.Tiendas.First(p => p.nombre == name);
+                }
+                return null;
+            }
+        }
+
+        #endregion
 
         #region Atributos
 
@@ -44,8 +59,13 @@ namespace CheBay.Data
         {
             using (var context = new InheritanceMappingContext())
             {
-                context.Atributos.Attach(atr);
-                context.SaveChanges();
+                Atributo aux = (context.Atributos.First(p => p.id == atr.id));
+
+                if (atr != null)
+                {
+                    context.Entry(aux).CurrentValues.SetValues(atr);
+                    context.SaveChanges();
+                }
             }
         }
 
@@ -73,5 +93,85 @@ namespace CheBay.Data
 
         #endregion
 
+        #region Products
+
+        public static List<Producto> GetProducts()
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                return context.Productos.ToList();
+            }
+        }
+
+        public static void DeleteProduct(int id)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                Producto prd = GetProducto(id);
+                context.Productos.Attach(prd);
+                context.Productos.Remove(prd);
+                context.SaveChanges();
+            }
+        }
+
+        public static void UpdateProduct(Producto prd)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                Producto aux = (context.Productos.First(p => p.id == prd.id));
+
+                if (prd != null)
+                {
+                    context.Entry(aux).CurrentValues.SetValues(prd);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public static Producto GetProducto(int id)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                if (context.Productos.Where(p => p.id == id).Count() > 0)
+                {
+                    return context.Productos.First(p => p.id == id);
+                }
+                return null;
+            }
+        }
+
+        public static void AddProducto(Producto prd)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                context.Productos.Add(prd);
+
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
+
+        #region Categories
+
+        public static List<Categoria> GetCategories()
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                return context.Categorias.ToList();
+            }
+        }
+
+        public static void AddCategoria(Categoria cat)
+        {
+            using (var context = new InheritanceMappingContext())
+            {
+                context.Categorias.Add(cat);
+
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
     }  
 }
